37b59d13e96f2d77235fb3b141215743c6240bc2
Illusix Liu <illuuuuuuuu@users.noreply.github.com>
2026-08-14 13:19:29 +0800

    Import mean inequalities and quadratics

--- HighSchoolMathLean.lean
+++ HighSchoolMathLean.lean
@@ -4,7 +4,9 @@ Copyright (c) 2026 Illusix Liu. All rights reserved.
 
 import HighSchoolMathLean.Algebra.EqualityAndOrder
 import HighSchoolMathLean.Algebra.Inequalities
+import HighSchoolMathLean.Algebra.MeanInequalities
 import HighSchoolMathLean.Algebra.Means
+import HighSchoolMathLean.Algebra.Quadratics
 import HighSchoolMathLean.Logic.Basic
 import HighSchoolMathLean.SetTheory.BasicRelations
 import HighSchoolMathLean.SetTheory.RelativeComplement
