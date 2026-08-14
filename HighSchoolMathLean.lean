0ab872ec994aebf56d52c9d88ce75b9a23e5f99e
Illusix Liu <illuuuuuuuu@users.noreply.github.com>
2026-08-14 12:12:12 +0800

    Import set lattice laws

--- HighSchoolMathLean.lean
+++ HighSchoolMathLean.lean
@@ -4,3 +4,4 @@ Copyright (c) 2026 Illusix Liu. All rights reserved.
 
 import HighSchoolMathLean.SetTheory.BasicRelations
 import HighSchoolMathLean.SetTheory.RelativeComplement
+import HighSchoolMathLean.SetTheory.SetLaws
