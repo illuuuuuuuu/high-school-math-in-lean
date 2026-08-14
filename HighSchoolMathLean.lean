4c64f46145e5fc6d6b29f5f67408f18f091c050e
Illusix Liu <illuuuuuuuu@users.noreply.github.com>
2026-08-14 13:47:06 +0800

    Import quadratic results and function rules

--- HighSchoolMathLean.lean
+++ HighSchoolMathLean.lean
@@ -7,6 +7,7 @@ import HighSchoolMathLean.Algebra.Inequalities
 import HighSchoolMathLean.Algebra.MeanInequalities
 import HighSchoolMathLean.Algebra.Means
 import HighSchoolMathLean.Algebra.Quadratics
+import HighSchoolMathLean.Functions.Rules
 import HighSchoolMathLean.Logic.Basic
 import HighSchoolMathLean.SetTheory.BasicRelations
 import HighSchoolMathLean.SetTheory.RelativeComplement
