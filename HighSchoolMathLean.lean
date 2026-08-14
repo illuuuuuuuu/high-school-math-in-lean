e668fa8c30cfdc7063400b1bb7df34aa83b2ccb2
Illusix Liu <illuuuuuuuu@users.noreply.github.com>
2026-08-14 12:31:07 +0800

    Import logic and algebra foundations

--- HighSchoolMathLean.lean
+++ HighSchoolMathLean.lean
@@ -2,6 +2,8 @@
 Copyright (c) 2026 Illusix Liu. All rights reserved.
 -/
 
+import HighSchoolMathLean.Algebra.EqualityAndOrder
+import HighSchoolMathLean.Logic.Basic
 import HighSchoolMathLean.SetTheory.BasicRelations
 import HighSchoolMathLean.SetTheory.RelativeComplement
 import HighSchoolMathLean.SetTheory.SetLaws
