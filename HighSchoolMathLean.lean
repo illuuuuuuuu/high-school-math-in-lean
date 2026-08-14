a298327a190e5d2e223c3523784fbaffb813256c
Illusix Liu <illuuuuuuuu@users.noreply.github.com>
2026-08-14 12:50:04 +0800

    Import inequalities and means

--- HighSchoolMathLean.lean
+++ HighSchoolMathLean.lean
@@ -3,6 +3,8 @@ Copyright (c) 2026 Illusix Liu. All rights reserved.
 -/
 
 import HighSchoolMathLean.Algebra.EqualityAndOrder
+import HighSchoolMathLean.Algebra.Inequalities
+import HighSchoolMathLean.Algebra.Means
 import HighSchoolMathLean.Logic.Basic
 import HighSchoolMathLean.SetTheory.BasicRelations
 import HighSchoolMathLean.SetTheory.RelativeComplement
