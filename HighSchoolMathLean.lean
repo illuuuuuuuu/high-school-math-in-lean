3572b396d7ad9192286efeade4f8f58ef341a804
Illusix Liu <illuuuuuuuu@users.noreply.github.com>
2026-08-24 14:18:16 +0800

    Import vector inner product and basis module

--- HighSchoolMathLean.lean
+++ HighSchoolMathLean.lean
@@ -23,6 +23,7 @@ import HighSchoolMathLean.PlaneVectors.Basic
 import HighSchoolMathLean.PlaneVectors.AdditionGeometry
 import HighSchoolMathLean.PlaneVectors.Operations
 import HighSchoolMathLean.PlaneVectors.LinearAndDirection
+import HighSchoolMathLean.PlaneVectors.InnerProductAndBasis
 import HighSchoolMathLean.Logic.Basic
 import HighSchoolMathLean.SetTheory.BasicRelations
 import HighSchoolMathLean.SetTheory.RelativeComplement
